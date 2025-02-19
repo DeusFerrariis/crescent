6875bba5f6211d142df1da32d2ec17d088b1c313
William Cleghorn IV <deusferrariis@proton.me>
2025-02-19 13:55:41 -0700

    had to play with wierd linking issues cause nix or smth

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    routes: Vec<Route>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Route {
+    path: String,
+    endpoint_url: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
