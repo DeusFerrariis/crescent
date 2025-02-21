2987e6740b30330c467e19f0af26c0d844e4d285
William Cleghorn IV <deusferrariis@proton.me>
2025-02-21 16:51:44 -0700

    catchup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use axum::{extract::Path, routing::get, Router};
+use axum::{extract::Path, http::HeaderMap, response::IntoResponse, routing::get, Router};
+use reqwest::header;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -22,17 +23,42 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn route_handler(Path(path_tail): Path<String>) -> String {
+async fn route_handler(Path(path_tail): Path<String>) -> impl IntoResponse {
     let cfg = std::fs::read_to_string("./config.toml").unwrap();
 
     let config: Config = toml::from_str(&cfg).unwrap();
     println!("Routes: {:?}", config.routes);
 
     for r in config.routes.iter() {
-        if r.path == "/".to_string() + &path_tail {
-            return r.endpoint_url.clone();
+        let path_parts: Vec<&str> = path_tail.split("/").collect();
+        let Some(root) = path_parts.get(0) else {
+            break;
+        };
+        if r.path == "/".to_string() + root {
+            let resp = reqwest::get(r.endpoint_url.as_str()).await;
+            let mut headers = HeaderMap::new();
+            let Ok(body) = resp else {
+                return (HeaderMap::new(), "fail".to_string());
+            };
+            let resp_headers = body.headers().clone();
+            for (k, v) in resp_headers {
+                let Some(hk) = k.clone() else {
+                    continue;
+                };
+                let header_key = hk.clone();
+                let Ok(header_value) = v.to_str() else {
+                    continue;
+                };
+                headers.insert(header_key, header_value.parse().unwrap());
+            }
+            if let Ok(text) = body.text().await {
+                return (headers.to_owned(), text);
+            }
+            return (HeaderMap::new(), r.endpoint_url.clone());
         }
     }
 
-    "not found".to_string()
+    return (HeaderMap::new(), "not found".to_string());
 }
+
+// NOTE: replace list of routes w/ b tree?
