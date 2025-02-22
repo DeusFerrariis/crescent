2cddd36faf99aa058aa1ac0ade2e29c293d1ea17
William Cleghorn IV <deusferrariis@proton.me>
2025-02-21 20:36:39 -0700

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,83 @@
-use axum::{extract::Path, http::HeaderMap, response::IntoResponse, routing::get, Router};
-use reqwest::header;
+use axum::{
+    extract::{Path, State},
+    http::HeaderMap,
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
 use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-struct Config {
-    pub routes: Vec<Route>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Route {
-    pub path: String,
-    pub endpoint_url: String,
-}
 
 #[tokio::main]
 async fn main() {
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
-    let app = Router::new().route("/api/{*wildcard}", get(route_handler));
+
+    let state = AppState::new().unwrap();
+    // TODO: live reload of config file
+
+    let app = Router::new()
+        .route("/api/{*wildcard}", get(route_handler))
+        .with_state(state);
 
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn route_handler(Path(path_tail): Path<String>) -> impl IntoResponse {
-    let cfg = std::fs::read_to_string("./config.toml").unwrap();
-
-    let config: Config = toml::from_str(&cfg).unwrap();
-    println!("Routes: {:?}", config.routes);
-
-    for r in config.routes.iter() {
-        let path_parts: Vec<&str> = path_tail.split("/").collect();
-        let Some(root) = path_parts.get(0) else {
-            break;
-        };
-        if r.path == "/".to_string() + root {
-            let resp = reqwest::get(r.endpoint_url.as_str()).await;
-            let mut headers = HeaderMap::new();
-            let Ok(body) = resp else {
-                return (HeaderMap::new(), "fail".to_string());
-            };
-            let resp_headers = body.headers().clone();
-            for (k, v) in resp_headers {
-                let Some(hk) = k.clone() else {
-                    continue;
-                };
-                let header_key = hk.clone();
-                let Ok(header_value) = v.to_str() else {
-                    continue;
-                };
-                headers.insert(header_key, header_value.parse().unwrap());
-            }
-            if let Ok(text) = body.text().await {
-                return (headers.to_owned(), text);
-            }
-            return (HeaderMap::new(), r.endpoint_url.clone());
-        }
-    }
-
-    return (HeaderMap::new(), "not found".to_string());
+#[derive(Clone, Debug)]
+struct AppState {
+    config: Config,
 }
 
-// NOTE: replace list of routes w/ b tree?
+impl AppState {
+    fn new() -> Result<AppState, ()> {
+        Ok(AppState {
+            config: load_config().unwrap(),
+        })
+    }
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct Config {
+    pub routes: Vec<Route>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct Route {
+    pub path: String,
+    pub endpoint_url: String,
+    // pub partial_path: Option<bool>,
+}
+
+impl Route {
+    async fn forward_request(&self) -> (HeaderMap, String) {
+        let Ok(resp) = reqwest::get(self.endpoint_url.as_str()).await else {
+            return (HeaderMap::new(), "fail".to_string());
+        };
+
+        let headers = resp.headers().clone();
+        let body = resp.text().await.unwrap();
+
+        return (headers, body);
+    }
+}
+
+async fn route_handler(
+    State(state): State<AppState>,
+    Path(path_tail): Path<String>,
+) -> impl IntoResponse {
+    match state
+        .config
+        .routes
+        .iter()
+        .find(|r| r.path == "/".to_string() + path_tail.as_str())
+    {
+        Some(route) => route.forward_request().await,
+        None => (HeaderMap::new(), "not_found".to_string()),
+    }
+}
+
+fn load_config() -> Result<Config, ()> {
+    let config_string = std::fs::read_to_string("./config.toml").unwrap();
+    let config: Config = toml::from_str(&config_string).unwrap();
+    return Ok(config);
+}
